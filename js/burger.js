e3e2e64c5b343b906bc0b93738c3198e23f012b7
Sergio <7ergio@gmail.com>
2021-08-27 17:59:12 +0200

    Сreated burger and modal files in js

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,3 @@
-// Burger
-
 const openMobileMenuButton = document.querySelector('.open-mobile-menu-js');
 const closeMobileMenuButton = document.querySelector('.close-mobile-menu-js');
 const mobileMenu = document.querySelector('.header-right');
@@ -18,17 +16,3 @@ function hideMobileMenu(){
     closeMobileMenuButton.style.display = 'none';
     mobileMenu.style.display = 'none';
 }
-
-
-// Modal window
-const modalWindow = document.querySelector('.modal-age-wrapper');
-const closeModalWindowButton = document.querySelector('.close-modal-js');
-const body = document.querySelector('body');
-
-closeModalWindowButton.addEventListener('click', closeModalWindow);
-
-function closeModalWindow(){
-    modalWindow.style.display = "none";
-    body.classList.remove("scroll-disabled");
-    
-}    
